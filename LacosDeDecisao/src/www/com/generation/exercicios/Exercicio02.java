eb59ca1e3aaab86b7c91d7eb0129a54aa5f9f688
igoryouiti <igoryouiti@hotmail.com>
2022-07-08 17:21:59 -0300

    add loop in main, then they can restart the input after de output. Delete test code

--- LacosDeDecisao/src/www/com/generation/exercicios/Exercicio02.java
+++ LacosDeDecisao/src/www/com/generation/exercicios/Exercicio02.java
@@ -10,83 +10,49 @@ public class Exercicio02 {
 		 */
 		
 		int number1, number2, number3;
-		int biggestNumber, middleNumber, smallestNumber;
+
 		
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Digite o primeiro número inteiro: ");
-		number1 = input.nextInt();
+		do {
 		
-		System.out.println("Digite o segundo número inteiro: ");
-		number2 = input.nextInt();
-		
-		System.out.println("Digite o terceiro número inteiro: ");
-		number3 = input.nextInt();
-		
-		input.close();
-		
-		System.out.println("\n");
-		
-		if((number1 > number2 && number1 > number3) || (number1 == number2 && number2 == number3)) {
-			System.out.println("O maior numero digitado foi: " + number1);
-			biggestNumber = number1;
-		} else if(number2 > number3 || number2 == number3) {
-			System.out.println("O maior numero digitado foi: " + number2);
-			biggestNumber = number2;
-		} else {
-			System.out.println("O maior numero digitado foi: " + number3);
-			biggestNumber = number3;
-		}
-		
-		if((number1 < number2 && number1 < number3) || (number1 == number2 && number2 == number3)) {
-			System.out.println("O menor numero digitado foi: " + number1);
-			smallestNumber = number1;
-		} else if(number2 < number3 || number2 == number3) {
-			System.out.println("O menor numero digitado foi: " + number2);
-			smallestNumber = number2;
-		} else {
-			System.out.println("O menor numero digitado foi: " + number3);
-			smallestNumber = number3;
-		}
-		
-		
-		if((number1 < number2 && number1 > number3) || (number1 > number2 && number1 < number3) ||
-			(number1 == number2 && number2 == number3)) {
-			System.out.println("O numero do meio digitado foi: " + number1);
-			middleNumber = number1;
-		} else if((number2 < number3 && number2 > number1) || (number2 > number3 && number2 < number1) ||
-				number2 == number3) {
-			System.out.println("O numero do meio digitado foi: " + number2);
-			middleNumber = number2;
-		} else{
-			System.out.println("O numero do meio digitado foi: " + number3);
-			middleNumber = number3;
-		}
-		
-		
-		if(number1 < number2) {
-			if(number2 < number3) {
-				System.out.println("Numeros em ordem: " + number1 + " - " + number2 + " - " + number3);
-			} else if(number1 < number3) {
-				System.out.println("Numeros em ordem: " + number1 + " - " + number3 + " - " + number2);
-			} else {
-				System.out.println("Numeros em ordem: " + number3 + " - " + number1 + " - " + number2);
-			}
-		} else if(number2 < number3) {
-			if(number1 < number3) {
-				System.out.println("Numeros em ordem: " + number2 + " - " + number1 + " - " + number3);
-			} else {
-				System.out.println("Numeros em ordem: " + number2 + " - " + number3 + " - " + number1);
-			}
+			System.out.println("Digite o primeiro número inteiro ou zero para sair: ");
+			number1 = input.nextInt();
 			
-		} else {
-			System.out.println("Numeros em ordem: " + number3 + " - " + number2 + " - " + number1);
-		}
-		
-	
-		
-		
+			System.out.println("Digite o segundo número inteiro ou zero para sair: ");
+			number2 = input.nextInt();
+			
+			System.out.println("Digite o terceiro número inteiro ou zero para sair: ");
+			number3 = input.nextInt();
+			
+			
+			
+			System.out.println("\n");
+			
+			if(number1 != 0 && number2 != 0 && number3 != 0) {
+				if(number1 < number2) {
+					if(number2 < number3) {
+						System.out.println("Numeros em ordem: " + number1 + " - " + number2 + " - " + number3);
+					} else if(number1 < number3) {
+						System.out.println("Numeros em ordem: " + number1 + " - " + number3 + " - " + number2);
+					} else {
+						System.out.println("Numeros em ordem: " + number3 + " - " + number1 + " - " + number2);
+					}
+				} else if(number2 < number3) {
+					if(number1 < number3) {
+						System.out.println("Numeros em ordem: " + number2 + " - " + number1 + " - " + number3);
+					} else {
+						System.out.println("Numeros em ordem: " + number2 + " - " + number3 + " - " + number1);
+					}
+					
+				} else {
+					System.out.println("Numeros em ordem: " + number3 + " - " + number2 + " - " + number1);
+				}
+			}
+		}while(number1 != 0 && number2 != 0 && number3 != 0);
+		input.close();
+		System.out.println("Fechando...");
 	}
 
 }
